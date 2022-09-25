d9c21eb605e7cb569c8a861196d6bf4660b5a726
excelolatunbosun <excellence.olatunbosun@betpawa.com>
2022-09-25 13:06:23 +0100

    included validation for user details

--- src/main/java/com/in28mins/rest/webservices/restfulwebservices/Resource/UserResource.java
+++ src/main/java/com/in28mins/rest/webservices/restfulwebservices/Resource/UserResource.java
@@ -4,6 +4,7 @@ package com.in28mins.rest.webservices.restfulwebservices.Resource;
 import com.in28mins.rest.webservices.restfulwebservices.service.UserDaoService;
 import com.in28mins.rest.webservices.restfulwebservices.user.User;
 import com.in28mins.rest.webservices.restfulwebservices.user.UserNotFoundException;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -37,7 +38,7 @@ public class UserResource {
     }
 
     @PostMapping("/post")
-    public ResponseEntity<User> postOne(@RequestBody User user) {
+    public ResponseEntity<User> postOne(@Valid @RequestBody User user) {
         User savedUser = service.saveOne(user);
         URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(savedUser.getId()).toUri();
         return ResponseEntity.created(location).build();
